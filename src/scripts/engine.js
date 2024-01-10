c8d1ebcb8a6b1475e57a9bddbc3c74ad112772dc
Gabriel Aparecido da paixão <gabriel711906@hotmail.com>
2024-01-10 13:46:08 -0300

    first commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,83 @@
+const state = {
+  view: {
+    squares: document.querySelectorAll(".square"),
+    enemy: document.querySelector(".enemy"),
+    timeLeft: document.querySelector("#time-left"),
+    score: document.querySelector("#score"),
+    start: document.querySelector('.start-btn')
+  },
+  values: {
+    gameVelocity: 1000,
+    hitPosition: 0,
+    result: 0,
+    currentTime: 60
+  },
+  actions: {
+    timerId: null,
+    countDownTimerId: null
+  },
+};
+
+function countDown() {
+  state.values.currentTime--;
+  state.view.timeLeft.textContent = state.values.currentTime;
+ 
+  if (state.values.currentTime <= 0) {
+    clearInterval(state.actions.countDownTimerId);
+    clearInterval(state.actions.timerId);
+    alert("Game Over! O seu resultado foi: " + state.values.result);
+  }
+}
+
+function playSound(audioName) {
+  let audio = new Audio(`./src/audios/${audioName}.m4a`);
+  audio.volume = 0.2;
+  audio.play();
+}
+
+function randomSquare() {
+  state.view.squares.forEach((square) => {
+    square.classList.remove("enemy");
+  });
+
+  let randomNumber = Math.floor(Math.random() * 9);
+  let randomSquare = state.view.squares[randomNumber];
+  randomSquare.classList.add("enemy");
+  state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerHitBox() {
+  state.view.squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+      if (square.id === state.values.hitPosition) {
+        state.values.result++;
+        state.view.score.textContent = state.values.result;
+        state.values.hitPosition = null;
+        playSound("hit");
+      }
+    });
+  });
+}
+
+function resetState() {
+  state.values.currentTime = 60;
+  state.values.result = 0;
+  state.view.score.textContent = state.values.result;
+  clearInterval(state.actions.countDownTimerId);
+  clearInterval(state.actions.timerId);
+  state.actions.countDownTimerId = null;
+  state.actions.timerId = null;
+}
+
+function start(){
+  resetState();
+  state.actions.timerId = setInterval(randomSquare, 1000);
+  state.actions.countDownTimerId = setInterval(countDown, 1000);
+  addListenerHitBox();
+}
+
+function initialize(){
+  state.view.start.addEventListener("click", start);
+}
+
+initialize();
